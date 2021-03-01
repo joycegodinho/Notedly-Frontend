ef91d6eaa978cb3aa7afe13d5bbe96551ec6b7b8
Joyce Bosco <joycegodinhobosco@gmail.com>
2021-03-01 17:38:35 -0300

    front and end connection implemented

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
-import Pages from '/pages'
-import GlobalStyle from '/components/GlobalStyle'
+import GlobalStyle from '/components/GlobalStyle';
+import Pages from '/pages';
 
-const App = () => {
-    return(
-        <div>
+const uri = process.env.API_URI;
+const cache = new InMemoryCache();
+
+const client = new ApolloClient ({
+    uri,
+    cache,
+    connectToDevTools: true
+});
+
+const App = () => (
+        <ApolloProvider client={client}>
             <GlobalStyle />
             <Pages />  
-        </div>
+        </ApolloProvider>
     );
-};
+
 
 //to render our application within the element with an ID of root 
 ReactDOM.render(<App />, document.getElementById('root'));
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,38 @@
 import React from 'react';
-import Button from '../components/Button'
+import { useQuery, gql } from '@apollo/client'
+
+const GET_NOTES = gql`
+    query NoteFeed($cursor: String) {
+        noteFeed(cursor: $cursor) {
+            cursor
+            hasNextPage
+            notes {
+                id
+                createdAt
+                content
+                favoriteCount
+                author {
+                    username
+                    id
+                }
+            }
+        }
+    }
+
+`;
+
 
 const Home = () => {
+
+    const { data, loading, error, fetchMore } = useQuery(GET_NOTES);
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>Error!</p>
     return(
         <div>
-            <p>This is the Home page</p>
-            <Button>Click Me</Button>
+            {console.log(data)}
+            The data loaded
+            
         </div>
     );
 };
