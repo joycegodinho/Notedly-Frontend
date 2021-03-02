2065170cbc0dc503940006226b5583933a985745
Joyce Bosco <joycegodinhobosco@gmail.com>
2021-03-02 12:10:37 -0300

    basic stalying and dinamic queries implemented

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown'
+import { format } from 'date-fns'
+import styled from 'styled-components'
 
+const StyledNote = styled.article`
+    max-width: 800px;
+    margin: 0 auto;
+`;
+const MetaData = styled.div`
+    @media (min-width: 500px) {
+        display: flex;
+        align-items: top;
+    }
+`;
+const MetaInfo = styled.div`
+    padding-right: 1em;
+`;
+const UserActions = styled.div`
+    margin-left: auto;
+`;
 
 const Note = ({ note }) => {
 
     return(
-        <article>
-            {' '}{note.author.username} {note.createdAt} {note.favoriteCount}{' '}
+        <StyledNote>
+            <MetaData>
+                <MetaInfo>
+                <em>by</em> {note.author.username} <br />
+                {format(new Date(note.createdAt), 'MM dd yyyy')}
+                </MetaInfo>
+                <UserActions>
+                    <em>Favorites:</em> {note.favoriteCount}
+                </UserActions>
+            </MetaData>
             <ReactMarkdown source={note.content} />
-        </article>
-
+        </StyledNote>
     );
 };
 
